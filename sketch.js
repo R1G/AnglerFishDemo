5184141a602c3949ddd05f11471014c11e4a7cd3
RedHippi <32603448+RedHippi@users.noreply.github.com>
2020-09-22 21:34:06 -0400

    Added player Movement
    Have a ball that moves towards the player's mouse.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+/*
+    Constants for the player character
+*/ 
+const meter = 100;
+const playerSpeed = 2;
+
 function setup() {
     createCanvas(1280, 800);
     background('black');
 }
 
+let player = {
+    x:100,       //Start Position coordinates
+    y:100,
+    color: [150,0,0],
+
+    //Function used to update our player character in draw
+    update() { 
+        clear();
+        fill(this.color);
+        circle(this.x,this.y,50);
+
+        //Give the player velocity towards a mouse
+        if(mouseIsPressed && int(dist(x,y,mouseX,mouseY)) > 5) {
+            this.x += ((mouseX-this.x)/meter)*playerSpeed;
+            this.y += ((mouseY-this.y)/meter)*playerSpeed;
+        }
+    }
+}
+
+
+
 function draw() {
+    player.update();
 }
