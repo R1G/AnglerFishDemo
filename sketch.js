53d0574ebc267e67f7523b987d31e0fc7dbc1ce8
RedHippi <32603448+RedHippi@users.noreply.github.com>
2020-09-25 00:05:34 -0400

    Added Player Movement
    Player can now move seamlessly in space due to a dynamically updating player drawer!!! Feels great!

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
 // webcam input stored here
 const video = document.getElementById("video");
 
+//Map Variables
+var MapRects = [newWall(600,600,55,55),
+                newWall(50, 50, 40, 70),
+                newWall(300,300,100,90),
+                newWall(100,150,20,100),
+                ];
+
 //image assets
 let anglerfish_img, ocean_img;
 
 //initial position and color
 var expression_color = 'grey';
 var position = {x:0, y:0};
+var center;
 
 // Constants for the player character
 const meter = 100;
-const playerSpeed = 2;
+const playerSpeed = 1;
 
 // Constants for expression weights
 const happyWeight = 150;
@@ -59,25 +67,38 @@ video.addEventListener("playing", () => {
 function setup() {
     createCanvas(windowWidth, windowWidth);
     background('black');
+    center = { x: windowWidth/2, y: windowHeight/2};
+    player.x = center.x;
+    player.y = center.y;
 }
 
 // player class
-let player = {
-    x:100,       //Start Position coordinates
-    y:100,
+var player = {
+    x: 0,       //Start Position coordinates
+    y: 0,
     color: [150,0,0],
+
+    size: 50,
+
+    visual_range: 170,
 
     //Function used to update our player character in draw
     update() { 
         fill(this.color);
-        circle(this.x,this.y,50);
+        circle(center.x, center.y, this.size);
 
-        this.x += (position.x - this.x)/meter*playerSpeed;
-        this.y += (position.y - this.y)/meter*playerSpeed;
-        
-        //uncomment to switch to mouse control
-        //this.x += (mouseX - this.x)/meter*playerSpeed;
-        //this.y += (mouseY - this.y)/meter*playerSpeed;
+        //MOUSE CONTROLS
+        //var xdiff = (mouseX - center.x);
+        //var ydiff = (mouseY - center.y);
+
+        //CAMERA CONTROLS
+        var xdiff = (position.x - this.x);
+        var ydiff = (position.y - this.y);
+
+        if(abs(ydiff) > this.size || abs(xdiff) > this.size) {
+            this.x += xdiff/meter*playerSpeed;
+            this.y += ydiff/meter*playerSpeed;
+        }
 
         //Dani:light beam!
         //expression_color is a combo of which expressions were detected
@@ -85,16 +106,25 @@ let player = {
         fill(expression_color);
 
         //beam originates on player and faces mouse position
-        let angle = Math.atan2(mouseY-position.y, mouseX-position.x); 
-        translate(this.x, this.y);
+        let angle = Math.atan2(mouseY-(center.x - this.size/2)
+                            , mouseX-(center.y - this.size/2));
+        push();
+        translate(center.x,center.y);
         rotate(angle);
-        triangle(35, 0, 200, 50 , 200, -50);
+        triangle(this.size/2, 0, this.size/2 + this.visual_range, 50 , this.size/2 + this.visual_range, -50);
+        pop();
     }
 }
 
+
+
+
 function draw() {
     background('black');
+
     player.update();
+
+    updateMap();
 }
 
 // each expression makes up a channel in the resulting color
@@ -106,3 +136,29 @@ function expressions_to_color(expressions) {
     return color(red, green, blue);
 }
 
+//Map Drawing functions
+function updateMap() {
+    push();
+    translate(-player.x,-player.y);
+    for( let i = 0; i < MapRects.length; i++) {
+        MapRects[i].update();
+    }
+    pop();
+}
+
+//MapObject Generating Functions
+function newWall(x, y, width, height) {
+    var wall = {
+        x: x,
+        y: y,
+        w: width,
+        h: height,
+        color: [0,150,0],
+
+        update: function() {
+            fill(this.color);
+            rect(x,y,width,height);
+        }
+    }
+    return wall;
+}
